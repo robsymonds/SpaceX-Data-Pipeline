cb36644dd5ebd7cb5bf69fc92c1c40d854678b4d
robsymonds <rsymonds@gmail.com>
2025-02-03 21:13:17 -0500

    add first lesson files

---
+++ variables.py
@@ -0,0 +1,13 @@
+# Integer
+launch_count = 10
+
+# Float
+success_rate = .95
+
+# String
+rocket_name = "Falcon 9"
+
+# Bool
+is_active = True
+
+print(f"The {rocket_name} rocket has launched {launch_count} times. Launch success rate is currently {success_rate * 100} %.")
